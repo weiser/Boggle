ecbc86e91f79db98dbb40f3538b028c45fa3e084
David Weiser <davidann@gmail.com>
2011-06-17 21:36:39 -0700

    got the builder to pass it's tests correctly

--- Boggle/src/main/java/weiser/david/BoggleBoardMaker.java
+++ Boggle/src/main/java/weiser/david/BoggleBoardMaker.java
@@ -63,65 +63,102 @@ public class BoggleBoardMaker {
     int posy = -1;
     int letterIndex = 0;
 
-    List<Integer> visitedSpaces = new ArrayList<Integer>(25);
+    // a positive number indicates that a letter was placed on the space right
+    // now. a negative number means that the space was already occupied when we
+    // got here.
+    List<Integer> visitedSpaces = new ArrayList<Integer>(50);
 
     for (int x = 0; x < this.board.getWidth() && posx < 0 && posy < 0; x++) {
       for (int y = 0; y < this.board.getLength() && posx < 0 && posy < 0; y++) {
         if (this.board.getLetterAt(x, y) != null
-            && this.board.getLetterAt(x, y).equals(word.charAt(0))) {
+            && this.board.getLetterAt(x, y).equals(
+                String.valueOf(word.charAt(0)))) {
           posx = x;
           posy = y;
+          visitedSpaces.add(0 - calcIntegerAddress(posx, posy));
         }
       }
     }
 
     if (posx < 0 && posy < 0) {
       Random rand = new Random();
+      do{
       posx = rand.nextInt(this.board.getWidth());
       posy = rand.nextInt(this.board.getLength());
-
+      }while(this.board.getLetterAt(posx, posy) != null);
+      
+      visitedSpaces.add(calcIntegerAddress(posx, posy));
+      this.board.setPosition(posx, posy, String.valueOf(word
+          .charAt(letterIndex)));
+      System.out.println("Inserted: "+word.charAt(letterIndex)+"\n"+this.board.toString());
     }
 
-    visitedSpaces.add(calcIntegerAddress(posx, posy));
-
-    this.board
-        .setPosition(posx, posy, String.valueOf(word.charAt(letterIndex)));
     letterIndex++;
     int nextLetterAddress;
 
-    // if the next letter of the word is next to where we're starting at, follow
-    // the next letter (as long as we haven't gone past the end of the word)
-    while (letterIndex < word.length()
-        && (nextLetterAddress = addressOfLetterInWord(String.valueOf(word
-            .charAt(letterIndex)), posx, posy)) >= 0) {
-      visitedSpaces.add(nextLetterAddress);
-      posx = calcXCoord(nextLetterAddress);
-      posy = calcYCoord(nextLetterAddress);
-      letterIndex++;
-    }
+    boolean moreMoves = true;
+    // until we either finish placing the word, or fail to place the word
 
-    // need to put the word in empty spaces
-    while (letterIndex < word.length()
-        && (nextLetterAddress = addressOfNeighborEmptySpace(posx, posy)) >= 0) {
-      visitedSpaces.add(nextLetterAddress);
-      posx = calcXCoord(nextLetterAddress);
-      posy = calcYCoord(nextLetterAddress);
-      this.board.setPosition(posx, posy, String.valueOf(word
-          .charAt(letterIndex)));
-      letterIndex++;
+    while (moreMoves) {
+      if (letterIndex >= word.length()) {
+        System.out.println("placed: " + word +" board looks like: ");
+        System.out.println(this.board.toString());
+        return true;
+      }
+      // if the next letter of the word is next to where we're starting at, and
+      // we
+      // haven't visited the space before, follow the next letter
+      if ((nextLetterAddress = addressOfLetterInWord(String.valueOf(word
+          .charAt(letterIndex)), posx, posy)) >= 0
+          && !visitedSpaces.contains(0 - nextLetterAddress)
+          && !visitedSpaces.contains(nextLetterAddress)) {
+        posx = calcXCoord(nextLetterAddress);
+        posy = calcYCoord(nextLetterAddress);
+        visitedSpaces.add(0 - calcIntegerAddress(posx, posy));
+        letterIndex++;
+        continue;
+      }
+      // if we got here, we failed to create the word using alerady placed
+      // letters
+
+      // need to put the word in empty spaces, since there was not an already
+      // placed letter that worked for us
+      if ((nextLetterAddress = addressOfNeighborEmptySpace(posx, posy)) >= 0 && !visitedSpaces.contains(nextLetterAddress)) {
+        
+        visitedSpaces.add(nextLetterAddress);
+        posx = calcXCoord(nextLetterAddress);
+        posy = calcYCoord(nextLetterAddress);
+
+        this.board.setPosition(posx, posy, String.valueOf(word
+            .charAt(letterIndex)));
+        System.out.println("Inserted: "+word.charAt(letterIndex)+"\n"+this.board.toString());
+        letterIndex++;
+        continue;
+      }
+
+      // if we got here, we failed to find either an available letter to help
+      // us, or an empty space. We did not place this word.
+      moreMoves = false;
+
     }
 
     // at this point, we failed to place the word on the board. remove the
     // partial word from the board and return failure
-    if (letterIndex < word.length()) {
-      for (int integerAddress : visitedSpaces) {
+
+    for (int integerAddress : visitedSpaces) {
+      // integerAddress < 0 ==> the letter was already there, so we don't
+      // remove it.
+      if (integerAddress >= 0) {
         posx = calcXCoord(integerAddress);
         posy = calcYCoord(integerAddress);
+        String removed = this.board.getLetterAt(posx, posy);
+        System.out.println("Remove ("+word+") before: "+removed+"\n"+this.board.toString());
         this.board.setPosition(posx, posy, null);
+        System.out.println("After remove: "+removed+"\n"+this.board.toString());
       }
-      return false;
     }
-    return true;
+
+    return false;
   }
 
   /**
@@ -150,14 +187,15 @@ public class BoggleBoardMaker {
       return -1;
     else {
       Random rand = new Random();
-      return allEmptyNeighborSpaces.get(rand.nextInt(allEmptyNeighborSpaces
+      int integerAddress = allEmptyNeighborSpaces.get(rand.nextInt(allEmptyNeighborSpaces
           .size()));
+      return integerAddress==Integer.MAX_VALUE?0:integerAddress;
     }
 
   }
 
   /**
-   * given (posx,posy) coordinates, return the calcIntegerAddress of the
+   * given (posx,posy) coordinates, return the calcIntegerAddress of a random
    * neighbor space which contains letter.
    * 
    * @param letter
@@ -168,6 +206,7 @@ public class BoggleBoardMaker {
    */
   private int addressOfLetterInWord(String letter, int posx, int posy) {
     int[] neighborOffsets = { -1, 0, 1 };
+    List<Integer> allLetterNeighborSpaces = new ArrayList<Integer>(8);
     for (int x : neighborOffsets) {
       for (int y : neighborOffsets) {
         // ensure we're in bounds
@@ -175,22 +214,51 @@ public class BoggleBoardMaker {
             && y + posy < this.board.getLength() && y + posy >= 0) {
           if (this.board.getLetterAt(posx + x, posy + y) != null
               && this.board.getLetterAt(x + posx, y + posy).equals(letter))
-            return calcIntegerAddress(posx + x, posy + y);
+            allLetterNeighborSpaces.add(calcIntegerAddress(posx + x, posy + y));
         }
       }
     }
-    return -1;
+    if (allLetterNeighborSpaces.isEmpty())
+      return -1;
+    else {
+      Random rand = new Random();
+      int integerAddress =  allLetterNeighborSpaces.get(rand.nextInt(allLetterNeighborSpaces
+          .size()));
+      return integerAddress==Integer.MAX_VALUE?0:integerAddress;
+    }
   }
 
+  /**
+   * Also, if (0,0) is passed in, MAXINT is returned.
+   * @param posx
+   * @param posy
+   * @return
+   */
   public int calcIntegerAddress(int posx, int posy) {
+    if(posx == 0 && posy == 0)
+      return Integer.MAX_VALUE;
     return posx * this.board.getWidth() + posy;
   }
 
+  /**
+   * If MAXINT is passed in, this means that the coordinates are (0,0).  return 0 in that case.
+   * @param nextLetterAddress
+   * @return
+   */
   public int calcYCoord(int nextLetterAddress) {
+    if(nextLetterAddress == Integer.MAX_VALUE)
+      return 0;
     return nextLetterAddress % this.board.getLength();
   }
 
+  /**
+   * If MAXINT is passed in, this means that the coordinates are (0,0).  return 0 in that case.
+   * @param nextLetterAddress
+   * @return
+   */
   public int calcXCoord(int nextLetterAddress) {
+    if(nextLetterAddress == Integer.MAX_VALUE)
+      return 0;
     return nextLetterAddress / this.board.getWidth();
   }
 
